ff89187c5c2bec950d979cf4dff6434b8bd106b8
Niharika <niharika86074@gmail.com>
2024-02-14 11:58:12 +0530

    js

--- first.js
+++ first.js
@@ -1,1 +1,32 @@
 console.log("hello");
+let full_name = "tony stark";
+console.log(full_name);
+x = null;
+console.log(x);
+isFollow = true;
+console.log(isFollow);
+
+//---------------------------------------------
+
+let fullName ;
+let age = 24;
+let totalPrice = 1000;
+//const a; ----------------------//dont do this
+console.log(fullName);
+
+
+//----------------------------------------------------------------
+typeof full_name;
+let y = BigInt("1234");
+let z = Symbol("hii!");
+
+
+//=============================================
+
+const student = {
+  name: "Niharika",
+  age: 20,
+  cgpa: 8.8,
+  isPass: true
+};
+console.log(student["age"]);
